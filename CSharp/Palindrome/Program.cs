d5270a9185b4fafddd0a83975a5099b204a1cd0e
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2020-06-04 22:42:15 +0200

    Palindome numbers

---
+++ CSharp/Palindrome/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+// Get the number from user
+// Hold the number in temporary variable
+// Reverse the number
+// Compare the temporary number with reversed number
+// If both numbers are same, print palindrome number
+// Else print not palindrome number
+
+namespace Palindrome
+{
+    class Palindrome
+    {
+        int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+
+        public void getResult()
+        {
+            string NumberToString = Convert.ToString(number);
+            char[] stringToArray = NumberToString.ToCharArray();
+            Array.Reverse(stringToArray);
+            string NumReversed = new string(stringToArray);
+            int stringToNum = Convert.ToInt32(NumReversed);
+            if (stringToNum == number)
+            {
+                Console.WriteLine(stringToNum);
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(NumReversed);
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Palindrome num = new Palindrome();
+            Console.Write("Enter your number : ");
+            num.Number = Convert.ToInt32(Console.ReadLine());
+            num.getResult();
+        }
+    }
+}
