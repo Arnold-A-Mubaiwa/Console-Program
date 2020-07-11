4398d3cce7a245c9318f35168e39235612d84e06
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2020-07-11 22:22:54 +0200

    Adjustments

--- Java/Cars/Cars.java
+++ Java/Cars/Cars.java
@@ -1,18 +1,18 @@
 import java.util.*;
-
+import javax.swing.JOptionPane;
 class CarBrands {
     Scanner s = new Scanner(System.in);
 
-    public int BestK, SecondBestK, LeastBestK;
-    public String Best, SecondBest, LeastBest;
+    int BestK, SecondBestK, LeastBestK;
+    String Best, SecondBest, LeastBest;
     boolean answer;
     int correct = 0;
     int incorrect = 0;
 
     public void getBestCars() {
-        HashMap<Integer, String> CarModels = new HashMap<Integer, String>();
-        CarModels.put(1, "BMW");
-        CarModels.put(2, "VW");
+        final HashMap<Integer, String> CarModels = new HashMap<Integer, String>();
+        CarModels.put(1, "Honda");
+        CarModels.put(2, "Hyundai");
         CarModels.put(3, "Benz");
         CarModels.put(4, "Toyota");
         try {
@@ -28,85 +28,93 @@ class CarBrands {
             System.out.print("Least Best :");
             LeastBestK = s.nextInt();
             LeastBest = CarModels.get(LeastBestK);
-        } catch (Exception e) {
+        } catch (final Exception e) {
             System.out.println("Invalid argument provuded");
         }
         System.out.println("");
 
     }
 
-    public void ac(String key, Boolean value) {
+    private void getAnswer(final String key, final Boolean value) {
         try {
             System.out.print(key + " : ");
             answer = s.nextBoolean();
             if (answer == value) {
-                System.out.println("=================================================");
-                System.out.println("Your Answer is correct");
-                System.out.println("=================================================\n");
+                // System.out.println("=====================================================");
+                // System.out.println("Your Answer is correct");
+                System.out.println("=====================================================\n");
+                JOptionPane.showMessageDialog(null,"Your Answer is correct" );
                 correct += 1;
             } else {
-                System.out.println("=====================================================");
-                System.out.println("Your Answer is incorrect. The corrent answer is " + value);
+                // System.out.println("=====================================================");
+                // System.out.println("Your Answer is incorrect. The corrent answer is " + value);
                 System.out.println("=====================================================\n");
+                JOptionPane.showMessageDialog(null,"Your Answer is incorrect. The corrent answer is " + value);
                 incorrect += 1;
             }
-        } catch (Exception e) {
+        } catch (final Exception e) {
             System.out.println("Enter true or false");
         }
     }
 
     public void CarQuestions() {
-        HashMap<String, Boolean> BMW = new HashMap<String, Boolean>();
-        BMW.put("Engine Capacity", true);
-        BMW.put("Built year", false);
-        BMW.put("Rob", true);
+        final HashMap<String, Boolean> Honda = new HashMap<String, Boolean>();
+        Honda.put("Your Honda car should be serviced every 12 months or 12500 miles, whichever is sooner.", true);
+        Honda.put(" Honda OHV stands for Overhead value.", true);
+        Honda.put(" Honda OHC stands for Overhead Company", false);
+        Honda.put("Your Honda warranty is based on when your Honda engine was originally sold.", true);
 
-        HashMap<String, Boolean> VW = new HashMap<String, Boolean>();
-        VW.put("Engine Capacity", true);
-        VW.put("Built year", false);
-        VW.put("Rob", true);
+        final HashMap<String, Boolean> Hyundai = new HashMap<String, Boolean>();
+        Hyundai.put("Hyundai was founded in 1967 by Chung Ju-yung.", true);
+        Hyundai.put("The first Hyundai Accet was introduced in 1995.", false);
+        Hyundai.put("The pony was South Korea’s first mass-produced and exported Hyundai.", true);
+        Hyundai.put("The first car that Hyundai sold in the US was the 1986 Hyundai Excel, a subcompact four door hatchback or sedan.", true);
+        
+        final HashMap<String, Boolean> Benz = new HashMap<String, Boolean>();
+        Benz.put("Benz allows to book a test drive.", true);
+        Benz.put("Benz has a smart key feature.", true);
+        Benz.put("Benz originates from South Africa", false);
+        Benz.put("Benz's co-founder is Ms Maart", false);
 
-        HashMap<String, Boolean> Benz = new HashMap<String, Boolean>();
-        Benz.put("Engine Capacity", true);
-        Benz.put("Built year", false);
-        Benz.put("Rob", true);
-
-        HashMap<String, Boolean> Toyota = new HashMap<String, Boolean>();
+        final HashMap<String, Boolean> Toyota = new HashMap<String, Boolean>();
         Toyota.put("Toyota Motor company was established in?", true);
-        Toyota.put("The first generation of hilux started in?", false);
-        Toyota.put("Does toyota have a smart key feature?", true);
-        Toyota.put("Is the toyota logo known for it's three ovals?", true);
+        Toyota.put("The first generation of Hilux started production in 1968. ", true);
+        Toyota.put("Toyota has a smart key feature.", true);
+        Toyota.put("The Toyota logo is known worldwide as a combination of three ovals.", true);
 
-        String Ranks[] = { Best, SecondBest, LeastBest };
+        final String Ranks[] = { Best, SecondBest, LeastBest };
+        System.out.println("=====================================================");
+        System.out.println("==   ANSWER THE FOLLOWING WITH EITHER true/false   ==");
+        System.out.println("=====================================================\n");
         for (int c = 0; c < Ranks.length; c++) {
-            String currentRank = Ranks[c];
+            final String currentRank = Ranks[c];
             switch (currentRank) {
-                case "BMW":
-                    for (Map.Entry<String, Boolean> mapElement : BMW.entrySet()) {
-                        String Question = (String) mapElement.getKey();
-                        boolean Ans = ((boolean) mapElement.getValue());
-                        ac(Question, Ans);
+                case "Honda":
+                    for (final Map.Entry<String, Boolean> mapElement : Honda.entrySet()) {
+                        final String Question = (String) mapElement.getKey();
+                        final boolean Ans = ((boolean) mapElement.getValue());
+                        getAnswer(Question, Ans);
                     }
                     break;
-                case "VW":
-                    for (Map.Entry<String, Boolean> mapElement : VW.entrySet()) {
-                        String Question = (String) mapElement.getKey();
-                        boolean Ans = ((boolean) mapElement.getValue());
-                        ac(Question, Ans);
+                case "Hyundai":
+                    for (final Map.Entry<String, Boolean> mapElement : Hyundai.entrySet()) {
+                        final String Question = (String) mapElement.getKey();
+                        final boolean Ans = ((boolean) mapElement.getValue());
+                        getAnswer(Question, Ans);
                     }
                     break;
                 case "Benz":
-                    for (Map.Entry<String, Boolean> mapElement : Benz.entrySet()) {
-                        String Question = (String) mapElement.getKey();
-                        boolean Ans = ((boolean) mapElement.getValue());
-                        ac(Question, Ans);
+                    for (final Map.Entry<String, Boolean> mapElement : Benz.entrySet()) {
+                        final String Question = (String) mapElement.getKey();
+                        final boolean Ans = ((boolean) mapElement.getValue());
+                        getAnswer(Question, Ans);
                     }
                     break;
                 case "Toyota":
-                    for (Map.Entry<String, Boolean> mapElement : Toyota.entrySet()) {
-                        String Question = (String) mapElement.getKey();
-                        boolean Ans = ((boolean) mapElement.getValue());
-                        ac(Question, Ans);
+                    for (final Map.Entry<String, Boolean> mapElement : Toyota.entrySet()) {
+                        final String Question = (String) mapElement.getKey();
+                        final boolean Ans = ((boolean) mapElement.getValue());
+                        getAnswer(Question, Ans);
                     }
                     break;
             }
@@ -115,19 +123,19 @@ class CarBrands {
     }
 
     public void getTotal() {
-        System.out.println("\n=================================================");
+        System.out.println("\n=====================================================");
         System.out.println("Below are the marks obtained");
-        System.out.println("=================================================");
+        System.out.println("=====================================================");
         System.out.format("%8s%18s%18s", "Correct", "Incorrect","Total\n");
-        int total = correct+incorrect;
+        final int total = correct+incorrect;
         System.out.format("%3d%18d%18d", correct, incorrect,total);
         System.out.println();
     }
 }
 
 public class Cars {
-    public static void main(String[] args) {
-        CarBrands car = new CarBrands();
+    public static void main(final String[] args) {
+        final CarBrands car = new CarBrands();
         car.getBestCars();
         car.CarQuestions();
         car.getTotal();
